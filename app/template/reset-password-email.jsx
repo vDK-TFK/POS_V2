aea80f9be18948bf6ef462ea0b29dd634e4c452d
Josué Bonilla Soto <josueuniv09@gmail.com>
2024-11-26 21:56:56 -0600

    Prueba Correo

--- app/template/reset-password-email.jsx
+++ app/template/reset-password-email.jsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 
 export const ResetPasswordEmailTemplate = ({ correo, resetPasswordToken }) => {
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
     const resetLink = `${baseUrl}/auth/reset?token=${resetPasswordToken}`;
 
     return (
